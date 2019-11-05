f1725f47f36d2fbc2d58db44fb6f6713e3d48727
Grayson Robles <grayson04runner@gmail.com>
2019-11-05 10:24:15 -0700

    learned html and status

---
+++ app.js
@@ -0,0 +1,8 @@
+// alert("Now im=n nmy own file!")
+
+document.body.style.background = "black";
+
+setTimeout(() => {
+    // document.body.style.background = "white";
+    console.log("Hi");
+}, 2000);
